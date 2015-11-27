c00bb3522c091a4a9efd6eb00b7c6752b22414f8
SHEePYTaGGeRNeP <sheepytaggernep@gmail.com>
2015-11-27 20:33:07 +0100

    Added support for capturing 1 window. Check PLSNOPOSTERINO how to get window

--- src/InputHive/Classes/NativeWin32.cs
+++ src/InputHive/Classes/NativeWin32.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace InputHive.Classes
 {
+    using System.Drawing;
+    using System.Drawing.Imaging;
+
     class NativeWin32
     {
         public struct ProcessWindow
@@ -30,6 +33,31 @@ namespace InputHive.Classes
             string pLpWindowName // window name 
         );
 
+
+        [DllImport("user32.dll")]
+        public static extern bool GetWindowRect(IntPtr hWnd, out REKT pLpRekt);
+        /// <summary>
+        /// The PrintWindow win32 api will capture a window bitmap even if the window is covered by other windows or if it is off screen
+        /// </summary>
+        [DllImport("user32.dll")]
+        public static extern bool PrintWindow(IntPtr hWnd, IntPtr hdcBlt, int nFlags);
+
+        public static Bitmap PrintWindow(IntPtr hwnd)
+        {
+            REKT rc;
+            GetWindowRect(hwnd, out rc);
+
+            Bitmap bmp = new Bitmap(rc.Width, rc.Height, PixelFormat.Format32bppArgb);
+            Graphics gfxBmp = Graphics.FromImage(bmp);
+            IntPtr hdcBitmap = gfxBmp.GetHdc();
+
+            PrintWindow(hwnd, hdcBitmap, 0);
+
+            gfxBmp.ReleaseHdc(hdcBitmap);
+            gfxBmp.Dispose();
+
+            return bmp;
+        }
 
         [DllImport("user32.dll")]
         public static extern int SendMessage(
@@ -110,4 +138,154 @@ namespace InputHive.Classes
         public static extern int GetDesktopWindow();
 
     }
+
+    [StructLayout(LayoutKind.Sequential)]
+    public struct REKT
+    {
+        private int _left;
+        private int _top;
+        private int _right;
+        private int _bottom;
+
+        public REKT(REKT Rectangle) : this(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom)
+        {
+        }
+        public REKT(int Left, int Top, int Right, int Bottom)
+        {
+            this._left = Left;
+            this._top = Top;
+            this._right = Right;
+            this._bottom = Bottom;
+        }
+
+        public int X
+        {
+            get { return this._left; }
+            set
+            {
+                this._left = value;
+            }
+        }
+        public int Y
+        {
+            get { return this._top; }
+            set
+            {
+                this._top = value;
+            }
+        }
+        public int Left
+        {
+            get { return this._left; }
+            set
+            {
+                this._left = value;
+            }
+        }
+        public int Top
+        {
+            get { return this._top; }
+            set
+            {
+                this._top = value;
+            }
+        }
+        public int Right
+        {
+            get { return this._right; }
+            set
+            {
+                this._right = value;
+            }
+        }
+        public int Bottom
+        {
+            get { return this._bottom; }
+            set
+            {
+                this._bottom = value;
+            }
+        }
+        public int Height
+        {
+            get { return this._bottom - this._top; }
+            set
+            {
+                this._bottom = value + this._top;
+            }
+        }
+        public int Width
+        {
+            get { return this._right - this._left; }
+            set
+            {
+                this._right = value + this._left;
+            }
+        }
+        public Point Location
+        {
+            get { return new Point(this.Left, this.Top); }
+            set
+            {
+                this._left = value.X;
+                this._top = value.Y;
+            }
+        }
+        public Size Size
+        {
+            get { return new Size(this.Width, this.Height); }
+            set
+            {
+                this._right = value.Width + this._left;
+                this._bottom = value.Height + this._top;
+            }
+        }
+
+        public static implicit operator Rectangle(REKT Rectangle)
+        {
+            return new Rectangle(Rectangle.Left, Rectangle.Top, Rectangle.Width, Rectangle.Height);
+        }
+        public static implicit operator REKT(Rectangle Rectangle)
+        {
+            return new REKT(Rectangle.Left, Rectangle.Top, Rectangle.Right, Rectangle.Bottom);
+        }
+        public static bool operator ==(REKT Rectangle1, REKT Rectangle2)
+        {
+            return Rectangle1.Equals(Rectangle2);
+        }
+        public static bool operator !=(REKT Rectangle1, REKT Rectangle2)
+        {
+            return !Rectangle1.Equals(Rectangle2);
+        }
+
+        public override string ToString()
+        {
+            return "{Left: " + this._left + "; " + "Top: " + this._top + "; Right: " + this._right + "; Bottom: " + this._bottom + "}";
+        }
+
+        public override int GetHashCode()
+        {
+            return this.ToString().GetHashCode();
+        }
+
+        public bool Equals(REKT Rectangle)
+        {
+            return Rectangle.Left == this._left && Rectangle.Top == this._top && Rectangle.Right == this._right && Rectangle.Bottom == this._bottom;
+        }
+
+        public override bool Equals(object Object)
+        {
+            if (Object is REKT)
+            {
+                return this.Equals((REKT)Object);
+            }
+            else if (Object is Rectangle)
+            {
+                return this.Equals(new REKT((Rectangle)Object));
+            }
+
+            return false;
+        }
+    }
+
 }
